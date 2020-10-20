a474e9fc6b412a969eef0e80c35ebd11665d693f
Teddy Chao <d.zhao.1@bham.ac.uk>
2020-10-20 16:59:11 +0100

    Create mrs_writeDPT.m

---
+++ MRS_lib/io/mrs_writeDPT.m
@@ -0,0 +1,19 @@
+function mrs_writeDPT ( outputFile, spectrum )
+% MRS_WRITEDPT write a spectrum into this file as an outpufile.
+% 
+% mrs_writeDPT( outputFile, spectrum )
+%
+% ARGS :
+% outputFile = a basis file created (.RAW) 
+% spectrum = a spectrum to write into outputFile 
+%
+% 
+% EXAMPLE: 
+% >> mrs_writeDPT('Ala.dpt', dpt_spectrum);
+%
+% AUTHOR : Teddy Chao
+% PLACE  : Institute of Cancer and Genomic Sciences
+%
+% Copyright (c) 2020, University of Birmingham. All rights reserved.
+end
+
