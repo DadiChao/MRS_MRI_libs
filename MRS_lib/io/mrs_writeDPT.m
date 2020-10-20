100af1f0284759a6f1b941b498a9a102ea435cfa
Teddy Chao <d.zhao.1@bham.ac.uk>
2020-10-20 19:19:03 +0100

    New feature
    New function for saving as TARQUIN DPT

--- MRS_lib/io/mrs_writeDPT.m
+++ MRS_lib/io/mrs_writeDPT.m
@@ -16,5 +16,15 @@ function mrs_writeDPT ( outputFile, spectrum )
 %
 % Copyright (c) 2020, University of Birmingham. All rights reserved.
 
-
+	fid=fopen(outputFile,'w');
+		for i=1:numel(A)
+			if A{i+1} == -1
+				fprintf(fid,'%s',A{i});
+				break
+      else
+        fprintf(fid,'%s\n',A{i});
+			end
+    end
+	fclose(fid);
+    
 end
